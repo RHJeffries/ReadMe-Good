b17480e5d9a7071dffc0b6d8fe4dc250ee5aeb23
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-11-06 22:39:31 +1030

    added validation to inputs

--- index.js
+++ index.js
@@ -8,26 +8,53 @@ const questions = () => {
     type: 'input',
     message: 'What is title of your project?',
     name: 'title',
+    validate: titleInput => {
+      if (titleInput) {
+          return true;
+      } else {
+          console.log('Every good project needs a title');
+          return false;
+      }
+  }
   },
   {
     type: 'input',
     message: 'Describe  your project?',
     name: 'description',
-  },
-  {
-    type: 'confirm',
-    message: 'Table of Contents',
-    name: 'contents',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+          return true;
+      } else {
+          console.log('Let everyone know what the project is about');
+          return false;
+      }
+  }
   },
   {
     type: 'input',
     message: 'What needs to be done to install your project?',
     name: 'installation',
+    validate: installationInput => {
+      if (installationInput) {
+          return true;
+      } else {
+          console.log('Need to explain how to install the project');
+          return false;
+      }
+  }
   },
   {
     type: 'input',
     message: 'How to use your project?',
     name: 'usage',
+    validate: usageInput => {
+      if (usageInput) {
+          return true;
+      } else {
+          console.log('Need to explain how to use the project');
+          return false;
+      }
+  }
   },
   {
     type: 'list',
@@ -46,26 +73,66 @@ const questions = () => {
     type: 'input',
     message: 'Who contributed to your project?',
     name: 'contribution',
+    validate: contributionInput => {
+      if (contributionInput) {
+          return true;
+      } else {
+          console.log('You need to enter who contributed to the project');
+          return false;
+      }
+  }
   },
   {
     type: 'input',
     message: 'How can your project be tested?',
     name: 'tests',
+    validate: testInput => {
+      if (testInput) {
+          return true;
+      } else {
+          console.log('Info is needed if there are tests or not');
+          return false;
+      }
+  }
   },
   {
     type: 'input',
     message: 'What can you do if there is an issue?',
     name: 'questions',
+    validate: questionsInput => {
+      if (questionsInput) {
+          return true;
+      } else {
+          console.log('Need to let everyone know how to reach you');
+          return false;
+      }
+  }
   },
   {
     type: 'input',
     message: 'Enter your Github username',
     name: 'username',
+    validate: usernameInput => {
+      if (usernameInput) {
+          return true;
+      } else {
+          console.log('No need to be shy, let the reader know where to find you');
+          return false;
+      }
+  }
   },
   {
     type: 'input',
     message: 'Enter your email address',
     name: 'email',
+    validate: emailInput => {
+      if (emailInput) {
+          return true;
+      } else {
+          console.log('Let the reader know how to contact you');
+          return false;
+      }
+  }
   }
 
 ]);
@@ -74,12 +141,11 @@ const questions = () => {
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
-
 }
 
 // TODO: Create a function to initialize app
-function init() {questions()
-    // Use writeFileSync method to use promises instead of a callback function
+const init = () =>  {
+  questions()
      .then((answers) => fs.writeToFile('README.md', generateMarkdown(answers)))
       .then(() => console.log('Successfully wrote to index.html'))
       .catch((err) => console.error(err));
