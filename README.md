55ba5627e39cdb779975097c3dd0e40748c7a5a9
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-11-08 22:35:01 +1030

    undefined returns for licence and github link resolved

---
+++ README.md
@@ -0,0 +1,33 @@
+# new project
+  
+  ![licence: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]
+
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Questions](#questions)
+  
+  ## Description:
+  i can
+  
+  ## Installation:
+  whatever feels good
+  
+  ## Usage:
+  the way ou like it
+  
+  ## License:
+  Further details about the licence can be found here [licence: MPL 2.0](https://opensource.org/licenses/MPL-2.0)
+  
+  ## Contribution:
+  me
+  
+  ## Testing:
+  any you can 
+  
+  ## Questions:
+  - Github: [RHJeffries](https://github.com/RHJeffries)
+  - Email: l;kdsfajglkfjg 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,93 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licence) {
+  if (licence === 'None'){
+    return '';
+  } 
+  
+   else if (licence === 'Apache') {
+    `![Licence](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  } 
+   
+   else if (licence === 'Eclipse'){
+   `![Licence](https://img.shields.io/badge/License-EPL%201.0-red.svg)`;
+  }
+  
+   else if (licence === 'GNU') {
+    return `![licence: GNU](https://img.shields.io/badge/license-GNU%20General%20Public%20License%20v3.0-green)`;
+  } 
+  
+  else if (licence === "ISC License (ISC)") {
+    return `![licence: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`
+  }
+
+   else if (licence === 'MIT') {
+    return `![licence: MIT](https://img.shields.io/badge/license-MIT-pink)`;
+
+  } 
+  
+  else if (licence === 'Mozilla') {
+    return `![licence: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`;
+
+  } 
+}
+
+renderLicenseBadge()
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licence) {
+if (licence === 'None'){
+  return '';
+}
+else if (licence === 'Apache') {
+  `[Apache licence](https://opensource.org/licenses/Apache-2.0)`;
+} 
+ 
+ else if (licence === 'Eclipse'){
+ `[Eclipse licence](https://opensource.org/licenses/EPL-1.0)`;
+}
 
+ else if (licence === 'GNU') {
+  return `[GNU licence](http://www.gnu.org/licenses/gpl-3.0)`;
+} 
+
+else if (licence === "ISC License (ISC)") {
+  return `[ISC licence](https://img.shields.io/badge/License-ISC-blue.svg)`
+}
+
+ else if (licence === 'MIT') {
+  return `[MIT licence](https://opensource.org/licenses/MIT)`;
+
+} 
+
+else if (licence === 'Mozilla') {
+  return `[MPL 2.0 licence](https://opensource.org/licenses/MPL-2.0)`;
+
+} 
+
+}
+renderLicenseLink()
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   
+  ${renderLicenseBadge(data.licence)}
+
   ## Table of Contents 
   - [Description](#description)
   - [Installation](#installation)
   - [Usage](#usage)
   - [Contribution](#contribution)
   - [Testing](#testing)
-  - [Additional Info](#additional-info)
+  - [Questions](#questions)
   
   ## Description:
   ${data.description}
@@ -32,16 +99,16 @@ function generateMarkdown(data) {
   ${data.usage}
   
   ## License:
-  ${data.licensing}
+  Further details about the licence can be found here ${renderLicenseLink(data.licence)}
   
   ## Contribution:
   ${data.contribution}
   
   ## Testing:
-  ${data.testing}
+  ${data.tests}
   
-  ## Additional Info:
-  - Github: [${data.github}](https://github.com/${data.github})
+  ## Questions:
+  - Github: [${data.username}](https://github.com/${data.username})
   - Email: ${data.email} `;
  
 }
